51e49099dc275937189f28b04328e54ab869cdb9
Дмитрий Евгеньевич Инденбом <indenbom.de@phystech.edu>
2021-10-20 16:19:48 +0300

    Скрипт приведён в рабочее состояние

--- 21.10.2021 - hw/Git diff/git_diff.sh
+++ 21.10.2021 - hw/Git diff/git_diff.sh
@@ -2,11 +2,11 @@
 
 # $1 - ссылка на удаленный репозиторий $2 -  имя ветки 1 $3 -  имя ветки 2
 
-mkdir .git_rep
-cd /.git_rep
+rm -rf .git_rep
 
-git clone $1
-git diff $2 $3 > ../diff.txt
+git clone $1 .git_rep
+cd .git_rep
+git diff origin/$2 origin/$3 > ../diff.txt
 
 cd ../
-rm -r .git_rep
+rm -rf .git_rep
